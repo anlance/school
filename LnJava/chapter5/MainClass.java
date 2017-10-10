8e6efffc06330c8131e7669f84b93724f9464bbd
anlan <834691974@qq.com>
2017-10-10 19:35:59 +0800

    删错了，重传

--- LnJava/chapter5/MainClass.java
+++ LnJava/chapter5/MainClass.java
@@ -3,7 +3,7 @@ package chapter5;
 public class MainClass{
     public static void main(String[] args){
         Dog dog = new Dog("uzi");
-        Cat cat = new Cat("麻辣香锅");
+        Cat cat = new Cat("麻辣香锅…");
         dog.showName();
         dog.cry();
         dog.swimming();
