3cf62deba56dfa408547457f3b1acc00657fac81
anlan <834691974@qq.com>
2017-10-30 22:56:33 +0800

    f

---
+++ WhiteBook/DoOne_OneDay/BellmanFord.cpp
@@ -0,0 +1,36 @@
+//-------1-------//
+#include<iosteam>
+using namespace std;
+
+const int INF = 0x3f3f3f;
+const int MAX_V = 100;
+const int MAX_E = 100;
+sturct edge{
+    int from;
+    int to;
+    int cost;
+};
+edge ee[MAX_E];
+int d[MAX_V];   //最短距离
+int num_v, num_e;
+
+bool Bellman(int s){
+    memset(d, INF, sizeof(d));
+    d[s] = 0;
+    int count = 0;  
+    while(true){
+        bool update = false;
+        for (int i = 0; i < num_e;i++){
+            edge et = ee[i];
+            if(d[et.from]!=INF&&d[et.to]>d[et.from]+et.cost){
+                d[et.to] = d[et.from] + et.cost;
+                update = true;
+            }
+        }
+        count++;
+        if(!update)
+            break;
+        if(j==v)
+            return false;   //负圈
+    }
+}
