24a1b6baffae0af1a305e4a4c156201ffb2493a8
anlan <834691974@qq.com>
2017-10-30 23:12:56 +0800

    t

--- WhiteBook/DoOne_OneDay/BellmanFord.cpp
+++ WhiteBook/DoOne_OneDay/BellmanFord.cpp
@@ -30,7 +30,29 @@ bool Bellman(int s){
         count++;
         if(!update)
             break;
-        if(j==v)
+        if(count==num_v)
             return false;   //负圈
+    }
+}
+
+///---------------2------------//
+bool Bellman2(int s){
+    memset(d, INF, sizeof(d));
+    d[s] = 0;
+    int count = 0;
+    while(true){
+        bool update = false;
+        for (int i = 0; i < num_e;i++){
+            edge et = ee[i];
+            if(d[et.from]!=INF&&d[et.to]>d[et.from]+et.cost){
+                d[et.to] = d[et.from] + et.cost;
+                update = true;
+            }
+        }
+        count++;
+        if(!upate)
+            break;
+        if(count==num_v)
+            return false;
     }
 }
