b58c0588009209fe2a1ddfb7e65b8090056f9e15
anlan <834691974@qq.com>
2017-10-31 09:05:30 +0800

    yesterday

--- WhiteBook/DoOne_OneDay/BellmanFord.cpp
+++ WhiteBook/DoOne_OneDay/BellmanFord.cpp
@@ -50,7 +50,7 @@ bool Bellman2(int s){
             }
         }
         count++;
-        if(!upate)
+        if(!update)
             break;
         if(count==num_v)
             return false;
@@ -59,3 +59,72 @@ bool Bellman2(int s){
 
 //git 出现了一个错  看一下解决没
 //再来
+
+//---------3--------//
+bool Bellamn3(int s){
+    memset(d, INF, sizeof(d));
+    d[s] = 0;
+    int count = 0;
+    while(true){
+        bool upadte = false;
+        for (int i = 0; i < num_e;i++){
+            edge et = ee[i];
+            if(d[et.from]!=INF&&d[et.to]>d[et.from]+et.cost){
+                et.to = d[et.from] + et.cost;
+                update = true;
+            }
+        }
+        count++;
+        if(!update)
+            break;
+        if(vount==num_v)
+            return false;
+    }
+}
+
+//------4------//
+bool Bellman4(int s){
+    memset(d, INF, sizeof(d));
+    d[s] = 0;
+    int count = 0;
+    while(true){
+        bool update = false;
+        for (int i = 0; i < num_e;i++){
+            edge et = ee[i];
+            if(d[et.from]!=INF&&d[et.to]>d[et.from]+et.cost){
+                d[et.to] = d[et.from] + et.cost;
+                update = true;
+            }
+        }
+        count++;
+        if(!upadte)
+            break;
+        if(count==num_v)
+            return false;
+    }
+    return true;
+}
+
+//-----5--------//
+
+bool Bellman5(int s){
+    memset(d, INf, sizeof(d));
+    d[s] = 0;
+    int count = 0;
+    while(true){
+        bool update = false;
+        for (int i = 0; i < num_e;i++){
+            edge et = ee[i];
+            if(d[et.from]!=INf&&d[et.to]>d[et.from]+et.cost){
+                d[et.to] = d[et.from] + et.cost;
+                update = true;
+            }
+        }
+        count++;    //可以把update与count结合成一个东西,但效果并不那么好
+        if(!update)
+            break;
+        if(count==num_v)
+            return false;
+    }
+    return true;
+}
