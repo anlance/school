9d88100fac925dd902200425dceea6938d540e6b
anlan <anlan1996@qq.com>
2018-08-30 22:16:57 +0800

    start luogu

---
+++ luogu/java/P1001.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         System.out.println(a+b);
+//     }
+// }
+
+public class P1001 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
