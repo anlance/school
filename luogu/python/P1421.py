70f3eeabc9b796deb5c3cf77c23ce2d5a379311e
anlan <anlan1996@qq.com>
2018-09-18 17:19:04 +0800

    del

--- luogu/python/P1421.py
+++
@@ -1,4 +0,0 @@
-s = input().split()
-temp = int(s[0])*10 + int(s[1])
-ans = temp//19
-print(ans)
--- luogu/python/P1423.py
+++
@@ -1,9 +0,0 @@
-s = float(input())
-n = 1
-start = 2.0
-temp = 2.0
-while (temp<s):
-    start*=0.98
-    temp+=start
-    n+=1
-print(n)
