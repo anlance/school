c156eddedcfed19ee740fdb9aee659868443843f
anlan <anlan1996@qq.com>
2018-11-10 16:44:03 +0800

    静态创建和使用Fragment

--- advancedWeb/README.md
+++ advancedWeb/README.md
@@ -2,3 +2,4 @@
 ## 实验目录：
 
 + [第二次实验](https://github.com/anlance/anlance/tree/eda7424f0069c427ea4ded19d85525e662e6fb6b) : 验证&国际化
++ [第三次实验](https://github.com/anlance/anlance/tree/997016165ea1d94379e4bce0b702219d9aaca990) : hibernate & 注册登录 & 暗文存储密码
--- app/src/main/java/club/anlan/second/MainActivity.java
+++ app/src/main/java/club/anlan/second/MainActivity.java
@@ -1,30 +1,38 @@
 package club.anlan.second;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button buttonGotoSign;
+    private ImageView bigImage;
+    private ImageView smallImage;
+    int imgID[]={R.id.small_3_1,R.id.small_3_2,R.id.small_3_3,R.id.small_3_4,R.id.small_3_5,R.id.small_3_6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonGotoSign = (Button) findViewById(R.id.sign);
+        bigImage = (ImageView)findViewById(R.id.big_image);
+        showImage();
+    }
 
-        // 点击进入下一页
-        buttonGotoSign.setOnClickListener(new View.OnClickListener(){
+    protected void showImage(){
+        for(int i=0;i<imgID.length;i++){
+            smallImage = (ImageView)findViewById(imgID[i]);
+            smallImage.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, NoteActivity.class);
-                startActivity(intent);
-            }
-        });
+                @Override
+                public void onClick(View v) {
+                    ImageView img = (ImageView) v;
+                    bigImage.setImageDrawable(img.getDrawable());
+                }
+            });
+        }
     }
 
 
